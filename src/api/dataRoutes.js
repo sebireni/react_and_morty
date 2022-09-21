0bcf20f6ca4eb7738b241a1269defdc9f932c733
sebireni <sebireni@gmail.com>
2022-09-21 10:54:18 +0200

    filesdeleted

--- src/api/dataRoutes.js
+++
@@ -1,6 +0,0 @@
-// pre-existing file
-
-export const mainUrls = {
-  characters: "https://rickandmortyapi.com/api/character/?page=",
-  locations: "https://rickandmortyapi.com/api/location/?page=",
-};
